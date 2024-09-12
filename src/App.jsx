a9839b9c6f6609db00fa0808c1a0150a97e71a70
fareedmirzay <fareedmirzay@ymail.com>
2024-09-12 15:53:07 -0400

    new projects updates

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+
+function App() {
+  return (
+    <>
+          <Header />
+          <main>
+              <Article />
+            
+              <Article1 />
+          </main>
+          <Footer />
+          </>
+  );
+}
+
+export default App;
+
+export default App
